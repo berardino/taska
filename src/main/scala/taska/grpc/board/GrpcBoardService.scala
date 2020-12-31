aeee6c7c66e20d113e942cec41dd9f50d54b3e11
Berardino la Torre <berardino.latorre@gmail.com>
2020-12-30 22:26:42 -0800

    Complete GrpBoardService implementation

--- src/main/scala/taska/grpc/board/GrpcBoardService.scala
+++ src/main/scala/taska/grpc/board/GrpcBoardService.scala
@@ -3,10 +3,12 @@ package taska.grpc.board
 import akka.util.Timeout
 import io.grpc.ServerServiceDefinition
 import org.springframework.stereotype.Component
-import taska.entity.board.BoardCommand.CreateBoard
+import taska.entity.board.BoardCommand._
 import taska.entity.board.BoardEntitySharding
 import taska.grpc.GrpcService
 import taska.proto.board.BoardServiceGrpc.BoardService
+import taska.proto.board.UpdateBoardReq.Update.Update.{Description, Title}
+import taska.proto.board.UpdateBoardReq.{UpdateDescription, UpdateTitle}
 import taska.proto.board._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -33,10 +35,44 @@ class GrpcBoardService(
       .map(_ => CreateBoardRes())
   }
 
-  override def archive(req: ArchiveBoardReq): Future[ArchiveBoardRes] = ???
+  override def archive(req: ArchiveBoardReq): Future[ArchiveBoardRes] = {
+    entity
+      .runCommand(
+        req.id,
+        replyTo => ArchiveBoard(replyTo)
+      )
+      .map(_ => ArchiveBoardRes())
+  }
 
-  override def unArchive(req: UnArchiveBoardReq): Future[UnArchiveBoardRes] =
-    ???
+  override def unArchive(req: UnArchiveBoardReq): Future[UnArchiveBoardRes] = {
+    entity
+      .runCommand(
+        req.id,
+        replyTo => UnArchiveBoard(replyTo)
+      )
+      .map(_ => UnArchiveBoardRes())
+  }
 
-  override def update(req: UpdateBoardReq): Future[UpdateBoardRes] = ???
+  override def update(req: UpdateBoardReq): Future[UpdateBoardRes] = {
+    entity
+      .runCommand(
+        req.id,
+        replyTo => UpdateBoard(replyTo, mapToEntityUpdates(req))
+      )
+      .map(_ => UpdateBoardRes())
+  }
+
+  def mapToEntityUpdates(req: UpdateBoardReq): Seq[UpdateBoardCommand] = {
+    req.updates.map(_.update).flatMap {
+      case Title(UpdateTitle(title, _)) => {
+        Some(UpdateBoardTitle(title))
+      }
+      case Description(UpdateDescription(description, _)) => {
+        Some(UpdateBoardDescription(description))
+      }
+      case _ => {
+        None
+      }
+    }
+  }
 }
